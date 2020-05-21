300d2c0dc422160c2341afc01987b7e7011a79b0
e <1936141611@qq.com>
2020-05-21 13:41:30 +0800

    Boot

---
+++ src/main/java/com/cslg/schoolmall2/Schoolmall2Application.java
@@ -0,0 +1,13 @@
+package com.cslg.schoolmall2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schoolmall2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schoolmall2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cslg/schoolmall2/Schoolmall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cslg.schoolmall2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schoolmall2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
